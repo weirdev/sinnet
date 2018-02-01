8f568c2d691a84d52e49a1c47b09ad0bbc34a203
Sophia Mallaro <sophia-mallaro@uiowa.edu>
2018-02-01 15:13:02 -0600

    grab and throw objects

---
+++ Sinnet/Assets/Scripts/Racket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+	// Use this for initialization
+	private SteamVR_TrackedObject trackedObj;
+	// 2
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Controller.GetHairTriggerDown())
+		{
+			Debug.Log(gameObject.name + " Trigger Press");
+		}
+
+		// 3
+		if (Controller.GetHairTriggerUp())
+		{
+			Debug.Log(gameObject.name + " Trigger Release");
+		}
+	}
+}
